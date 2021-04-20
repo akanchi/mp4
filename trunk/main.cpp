42d64ead32cb2e70d73b5ff688fbffce0a24697c
akanchi <chenpx718@gmail.com>
2021-04-20 15:14:17 +0800

    update tasks.json

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -2,12 +2,26 @@
 #include <memory>
 #include <fstream>
 #include "mp4_demuxer.hpp"
+#include "simple_buffer/simple_buffer.h"
 
 int main() {
     std::cout << "mp4" << std::endl;
 
+    std::ifstream ifile("./docs/output.mp4", std::ios::binary | std::ios::in);
+
     Mp4Demuxer demuxer;
-    // demuxer.decode()
+    SimpleBuffer in;
+
+    char buffer[1024] = {};
+    while (!ifile.eof()) {
+        size_t count = ifile.read(buffer, 1024).gcount();
+        in.append(buffer, count);
+
+        int ret = demuxer.decode(&in);
+        if (ret == 0) {
+            in.clear();
+        }
+    }
 
     return 0;
 }
